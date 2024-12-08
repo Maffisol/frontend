f9c9b8d77c8858d4c0e5b232a52e90092699bf2c
maffisol <m.ertemel.91@gmail.com>
2024-12-08 14:57:02 +0100

    Updated README file

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate, Link } from 'react-router-dom';
 import { WalletModalProvider } from '@solana/wallet-adapter-react-ui';
 import { useWallet } from '@solana/wallet-adapter-react';
-import './utils//polyfills'; // Import the polyfills before any other logic
+import './utils/polyfills'; // Import the polyfills before any other logic
 import axios from 'axios'; // Import axios voor API-verzoeken
 import WalletConnect from './components/WalletConnect';
 import Clicker from './components/Clicker';
